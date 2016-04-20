1ff5da17e70e6c7033f007281f5429c78a6b3528
Gokul <gokrazzzy@gmail.com>
2016-04-20 11:49:55 +0800

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,42 @@
+var plotterApp = angular.module('plotterApp', ['chart.js']);
+
+plotterApp.controller('plotterCtrl', function ($scope, $http, plotterHelper){
+	var pctrl = this;
+	pctrl.resultData = [];
+	
+	$scope.onClick = function (points, evt) {
+		console.log(points, evt);
+	};
+	
+	$scope.submit = function(){
+		buildParams='getMinuteVwap[]';
+		$http({
+		  method: 'GET',
+		  url: '/ipc?0!'+buildParams
+		}).success(function(data, status){
+
+		$scope.resultData = deserialize(plotterHelper.getBuff(data));
+		
+		$scope.labels = _.pluck($scope.resultData, 'minute');
+		$scope.series = ['Vwap', 'AvgPrice'];
+		$scope.data = [_.pluck($scope.resultData, 'vwap'),_.pluck($scope.resultData, 'avg_px')];
+		
+        pctrl.resultData = $scope.resultData;
+		});
+	};	
+});
+	
+plotterApp.factory('plotterHelper', function(){
+    function ipcstr2arraybuffer(str){
+      var buffer = new ArrayBuffer(str.length/2);
+      var bufferView = new Uint8Array(buffer);
+      for(var i = 0; i < buffer.byteLength; i++){
+        bufferView[i] = parseInt("0x"+str.substr(2*i,2));
+      };
+      return buffer;
+    };
+
+    return{
+        getBuff: ipcstr2arraybuffer
+    };
+});
